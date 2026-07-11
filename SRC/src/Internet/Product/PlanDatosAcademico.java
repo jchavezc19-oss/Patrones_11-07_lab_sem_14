305a5aa6743cb8b795d41bcd15d655dcfd53c691
LEONARDO CHAVEZ CASTILLO <jchavezc19@upao.edu.pe>
2026-07-11 12:43:56 -0500

    implementa subclase PlanDatosAcademico

---
+++ SRC/src/Internet/Product/PlanDatosAcademico.java
@@ -0,0 +1,8 @@
+// Autor: Chavez Castillo Leonardo
+package Internet.Product;
+
+public class PlanDatosAcademico extends PlanDatos { // Hereda de la clase abstracta PlanDatos
+    public PlanDatosAcademico() { // Constructor de la clase
+        this.nombre = "Plan Académico"; // Asigna el nombre específico de este tipo de producto
+    }
+}
