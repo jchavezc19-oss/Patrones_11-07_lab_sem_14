9e6910322da11696097024962983514bf7615777
LEONARDO CHAVEZ CASTILLO <jchavezc19@upao.edu.pe>
2026-07-11 12:45:39 -0500

    IMplemento subclase Plan Datos Pesonal

---
+++ SRC/src/Internet/Product/PlanDatosPersonal.java
@@ -0,0 +1,8 @@
+// Autor: Chavez Castillo Leonardo
+package Internet.Product;
+
+public class PlanDatosPersonal extends PlanDatos { // Hereda de la clase abstracta PlanDatos
+    public PlanDatosPersonal() { // Constructor de la clase
+        this.nombre = "Plan Personal"; // Asigna el nombre específico
+    }
+}
