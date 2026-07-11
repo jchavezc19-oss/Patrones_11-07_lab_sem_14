5aade5b2e35853b2cadc8a1f9227de8ff3c36bc6
LEONARDO CHAVEZ CASTILLO <jchavezc19@upao.edu.pe>
2026-07-11 13:01:02 -0500

    Implemento Subclase Plan Datos Negocio

---
+++ SRC/src/Internet/Product/PlanDatosNegocio.java
@@ -0,0 +1,8 @@
+// Autor: Chavez Castillo Leonardo
+package Internet.Product;
+
+public class PlanDatosNegocio extends PlanDatos { // Hereda de la clase abstracta PlanDatos
+    public PlanDatosNegocio() { // Constructor de la clase
+        this.nombre = "Plan Negocio"; // Asigna el nombre específico
+    }
+}
